6b862d20a83abc7080b2adcfdf3eb1dad74f5267
randika96 <rpjayasekara.16@cse.mrt.ac.lk>
2018-04-20 09:28:03 +0530

    UI improvements admin/dashbord and authetication configuration

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
-var express = require("express");
-var app = express();
+var express    = require("express"),
+    bodyparser = require("body-parser"),
+    mongoose   = require("mongoose"),
+    passport   = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User        = require("./models/user"),
+    Admin       = require("./models/admin"),
+    seedDB     = require("./seeds");
 
+
+var app = express();
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({extended: true}));
+app.set('view engine',"ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+// connect with DB
+mongoose.connect("mongodb://localhost/SMS");
+
+//==========Passport configuration=============
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// seed the db
+
+seedDB();
+
 
 app.get("/",function (req,res) {
-    res.render("login");
+    res.render("admin/index");
 })
+
+app.get("/dashbord",function (req,res) {
+    res.render("admin/index");
+    // console.log(req);
+    // console.log(req.user.isUser==true);
+    Admin.findOne({"authent.id":req.user._id},function (err,admin) {
+        if(err){
+            console.log(err);
+        }else {
+            console.log(admin);
+        }
+    })
+
+})
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/dashbord",
+        failureRedirect: "/"
+    }), function(req, res){
+});
 
 
 app.listen(3000,function () {
---
+++ seeds.js
@@ -0,0 +1,49 @@
+var mongoose = require("mongoose");
+var passport = require("passport");
+var User = require("./models/user");
+var Admin   = require("./models/admin");
+
+
+var data =  {
+    name: "Admin",
+    email: "admin@SMS.com"
+    };
+
+
+//====Create admin========
+
+function seedDB(){
+  User.remove({},function (err) {
+      if(err){
+          console.log(err);
+      }else{
+          Admin.remove({},function (err) {
+              if(err){
+                  console.log(err);
+              }else {
+                  User.register({username:"admin"}, "password", function(err, user){
+                      if(err){
+                          console.log(err);
+                      }else{
+                          Admin.create(data,function (err,admin) {
+                              if(err){
+                                        console.log(err);
+                                    }else{
+                                        admin.authent.id=user._id;
+                                        admin.authent.username=user.username;
+                                        admin.save();
+                                        console.log("Admin created!!!");
+                                    }
+                                })
+
+                      }
+
+                  });
+              }
+          })
+      }
+  })
+
+}
+
+module.exports = seedDB;
